6bfadb2c7e49b0787fee4180fc6ead81bf0f9a80
XintianWang <32439626+XintianWang@users.noreply.github.com>
2017-10-05 00:18:39 -0400

    Add files via upload

--- MapEditor.java
+++ MapEditor.java
@@ -1,185 +1,121 @@
-package team.groupfour.risk.mapeditor;
+package mapEditor;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
 
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.SpringLayout.Constraints;
-import javax.swing.border.Border;
-import javax.swing.border.MatteBorder;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 
-/**
- * This class is used to create a new map for the Risk. 
- * At the same time, player can also view their creation 
- * at the right side of window.
- * 
- * @author Jingyu Lu
- *
- */
-public class MapEditor extends JFrame{
+public class MapEditor extends JFrame {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	javax.swing.JButton newMapBtn;
+    javax.swing.JButton loadFromFileBtn;
+	javax.swing.JButton newContinentBtn;
+    javax.swing.JButton newCountryBtn;
+    javax.swing.JButton saveToFileBtn;
+    javax.swing.JButton exitBtn;
+    
+    JTree treeContinent;
+    
+    
+    public static void main(String[] args) {  
+    	MapEditor myMapEditor = new MapEditor();  
+    	myMapEditor.initGUI();  
+    }  
+    
+	public void initGUI(){  
+        setTitle("Map Editor - Untitled");  
+        setSize(1024,770);
+        int screenWidth = ((int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().width);
+        int screenHeight = ((int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().height);
+        setLocation((screenWidth-1024)/2, (screenHeight-770)/2);  
+        //set exit program when close the window  
+        setDefaultCloseOperation(3);  
+        //not capable adjust windows size  
+        setResizable(false);  
 
-	//new panels
-	JPanel myLeftPanel=new JPanel();
-	JPanel myMidTopPanel=new JPanel();
-	JPanel myMidBottomPanel=new JPanel();
-	JPanel myRightPanel=new JPanel();
-	
-//	JScrollPane midTopScrollPane=new JScrollPane();
-//	JScrollPane midBottomScrollPane=new JScrollPane();
-//	JScrollPane rightScrollPane=new JScrollPane();
-	
-	//components in panel
-	//leftPanel
-	//border used in JTextField
-	MatteBorder myBorder = new MatteBorder(0, 0, 1, 0, new Color(0, 0, 255));
-	//label
-	JLabel labelCountry=new JLabel("Add Country: ");
-	JLabel labelContinent=new JLabel("Add Continent: ");
-	JLabel labelConnection=new JLabel("Add Connection: ");		
-	//text
-	JTextField txtCountry=new JTextField("country");
-	JTextField txtContinent=new JTextField("continent");
-	JTextField txtConnection=new JTextField("connection");
-	//button
-	JButton btnAddCountry=new JButton("Add Country");
-	JButton btnAddContinent=new JButton("Add Continent");
-	JButton btnAddConnection=new JButton("Add Connection");
-	JButton btnSaveMap=new JButton("Save");
-	JButton btnLoadMap=new JButton("Load");
-	JButton btnExist=new JButton("Exist");
-	//midTopPanel
-	
-	//midBottomPanel
-	
-	//rightPanel
-//	//JTable
-//	//table header
-//	String[] right_table_header={"Country1", "Country2","Country3", "Country4", "Country5", "Country6"};
-//	//create new table with content
-//	JTable rightTable=new JTable(null, right_table_header);
-	
-	MapEditor(){
-		//layout
-		Container panelContainer=getContentPane();
-		panelContainer.setLayout(new GridLayout(1, 3, 3, 3));
-		
-		//leftPanel
-		//set and add components
-		myLeftPanel.setLayout(null);					//clean exist layout in layout manager
-		labelCountry.setBounds(30, 30, 160, 30);
-		txtCountry.setBounds(30, 61, 200, 20);
-		txtCountry.setBorder(myBorder);
-		btnAddCountry.setBounds(250, 61, 130, 25);
-				
-		labelContinent.setBounds(30, 100, 200, 30);
-		txtContinent.setBounds(30, 131, 200, 20);
-		txtContinent.setBorder(myBorder);
-		btnAddContinent.setBounds(250, 131, 130, 25);
-				
-		labelConnection.setBounds(30, 170, 200, 30);
-		txtConnection.setBounds(30, 201, 200, 20);
-		txtConnection.setBorder(myBorder);
-		btnAddConnection.setBounds(250, 201, 130, 25);
-		
-		myLeftPanel.add(labelCountry);
-		myLeftPanel.add(txtCountry);
-		myLeftPanel.add(btnAddCountry);
-		
-		myLeftPanel.add(labelContinent);
-		myLeftPanel.add(txtContinent);
-		myLeftPanel.add(btnAddContinent);
-		
-		myLeftPanel.add(labelConnection);
-		myLeftPanel.add(txtConnection);
-		myLeftPanel.add(btnAddConnection);
-		
-		btnSaveMap.setBounds(30, 600, 100, 25);
-		myLeftPanel.add(btnSaveMap);
-		btnSaveMap.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// 存储编辑好的地图，未完成
-				
-			}
-		});
-		
-		btnLoadMap.setBounds(130, 500, 100, 25);
-		myLeftPanel.add(btnLoadMap);
-		btnLoadMap.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				//create file loader
-				JFileChooser file_loader=new JFileChooser();
-				//set title 
-				file_loader.setDialogTitle("Please Select A Map!");
-				//file chooser
-				int selection_validation=file_loader.showOpenDialog(null);
-				//judge selected a map or not
-				if(JFileChooser.APPROVE_OPTION==selection_validation){
-					//file directory
-					File file_directory=file_loader.getCurrentDirectory();
-					File file_src=file_loader.getSelectedFile();
-					System.out.println(file_directory);
-					System.out.println(file_src);
-				}
-				
-			}
-		});
-		
-		btnExist.setBounds(230, 600, 100, 25);
-		myLeftPanel.add(btnExist);
-		btnExist.addActionListener(new ActionListener() {
-			//close map editor
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-				
-			}
-		});
-		
-		//midTopPanel
-		myMidTopPanel.setBackground(Color.green);
-		//midBottomPanel
-		myMidBottomPanel.setBackground(Color.yellow);
-		//rightPanel
-		myRightPanel.setBackground(Color.red);
-		
-		JSplitPane midTempPane=new JSplitPane(JSplitPane.VERTICAL_SPLIT, myMidTopPanel, myMidBottomPanel);
-		
-		panelContainer.add(myLeftPanel, new GridLayout(1, 1, 3, 3));
-		panelContainer.add(midTempPane, new GridLayout(1, 2, 3, 3));
-		panelContainer.add(myRightPanel, new GridLayout(1, 3, 3, 3));
-		
-		//general configuration
-		pack();
-		setSize(1200, 700);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		// test during coding only
-		new MapEditor();
-	}
+        //java.awt.FlowLayout fl = new java.awt.FlowLayout();   
+        setLayout(null);  
+        
+        
+        javax.swing.JLabel jlaName1 = new javax.swing.JLabel("Continents:");  
+        add(jlaName1);  
+        jlaName1.setFont(new java.awt.Font("dialog",1,16));
+        Dimension size = jlaName1.getPreferredSize();
+        jlaName1.setBounds(15,6,size.width+40,size.height);   
+        
+        DefaultMutableTreeNode myMap = new DefaultMutableTreeNode("Map - Untitled");
+        
+        DefaultMutableTreeNode asia = new DefaultMutableTreeNode("Continent - Asia");
+        
+        DefaultMutableTreeNode western = new DefaultMutableTreeNode("Continent - Conference");
+
+        DefaultMutableTreeNode pacific = new DefaultMutableTreeNode("Country - Pacific Division Teams");
+        
+        myMap.add(asia);
+        myMap.add(western);
+        western.add(pacific);
+        
+        treeContinent= new JTree(myMap);
+        
+        
+
+        JScrollPane scrollPane1= new JScrollPane(treeContinent,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        add(scrollPane1);  
+        scrollPane1.setBounds(15,30,300,660);	
+        
+        jlaName1.setText("Continents: totl");
+
+        
+        
+        javax.swing.JLabel jlaName2 = new javax.swing.JLabel("Countries and connections:");  
+        add(jlaName2);  
+        jlaName2.setFont(new java.awt.Font("dialog",1,16));
+        size = jlaName2.getPreferredSize();
+        jlaName2.setBounds(scrollPane1.getBounds().x+(int)(scrollPane1.getBounds().getWidth()),6,size.width,size.height);         
+        
+        JScrollPane scrollPane2= new JScrollPane(null,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        add(scrollPane2);  
+        scrollPane2.setBounds(scrollPane1.getBounds().x+(int)(scrollPane1.getBounds().getWidth()),30,690,660);
+        
+        newContinentBtn = new javax.swing.JButton("New Continent");
+        add(newContinentBtn);  	     
+        size = newContinentBtn.getPreferredSize();
+        size.width+=5;
+        size.height+=2;
+        newContinentBtn.setBounds(505,700,size.width,size.height);	        
+        setVisible(true);  
+        
+        newCountryBtn = new javax.swing.JButton("New Country");
+        add(newCountryBtn);  	     
+        //size = newCountry.getPreferredSize();
+        newCountryBtn.setBounds(newContinentBtn.getBounds().x+size.width+10,700,size.width,size.height);
+        
+        saveToFileBtn = new javax.swing.JButton("Save To File");
+        add(saveToFileBtn);  	     
+        //size = newCountry.getPreferredSize();
+        saveToFileBtn.setBounds(newCountryBtn.getBounds().x+size.width+10,700,size.width,size.height);
+        
+        exitBtn = new javax.swing.JButton("Exit");
+        add(exitBtn);  	     
+        //size = newCountry.getPreferredSize();
+        exitBtn.setBounds(saveToFileBtn.getBounds().x+size.width+10,700,size.width-15,size.height);        
+        
+        newMapBtn = new javax.swing.JButton("New Map");
+        add(newMapBtn);  	     
+        //size = newCountry.getPreferredSize();
+        newMapBtn.setBounds(15,700,size.width,size.height);
+        
+        loadFromFileBtn = new javax.swing.JButton("Load Exiting Map");
+        add(loadFromFileBtn);  	     
+        //size = newCountry.getPreferredSize();
+        loadFromFileBtn.setBounds(newMapBtn.getBounds().x+size.width+10,700,size.width+10,size.height);   
+        
+        setVisible(true);          
+    }
+
 }
