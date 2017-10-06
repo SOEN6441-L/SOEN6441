4b299a110767fc50b11464045b10f498501cad12
XintianWang <32439626+XintianWang@users.noreply.github.com>
2017-10-06 09:59:04 -0400

    modify some definition

--- src/mapelements/Continent.java
+++ src/mapelements/Continent.java
@@ -2,11 +2,9 @@ package mapelements;
 
 public class Continent {
 	public String continentName;
-	public int countryNum;
 	public int controlNum;
 	
 	public Continent(String name){
 		this.continentName = name;
-		countryNum = 0;
 	}
 }
