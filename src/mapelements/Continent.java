2482ad9227a3484bd5b5f0f590439367efaa6b9d
raphealshirley <32117886+raphealshirley@users.noreply.github.com>
2017-10-05 20:53:44 -0400

    Merge remote-tracking branch 'origin/XUEYING'
    continent

---
+++ src/mapelements/Continent.java
@@ -0,0 +1,12 @@
+package mapelements;
+
+public class Continent {
+	public String continentName;
+	public int countryNum;
+	public int controlNum;
+	
+	public Continent(String name){
+		this.continentName = name;
+		countryNum = 0;
+	}
+}
