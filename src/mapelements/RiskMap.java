e8aa0b46be24225d7c3807f57de94fa54c2e79a6
Nobitakun151 <32436934+LiChongWen@users.noreply.github.com>
2017-10-14 21:16:52 -0400

    RiskMap（FIx）

--- src/mapelements/RiskMap.java
+++ src/mapelements/RiskMap.java
@@ -10,103 +10,147 @@ import javax.swing.JOptionPane;
 public class RiskMap {
 	private int globalIndex;
 	public String riskMapName;
+	public String author;
+	public String warn,image,wrap,scroll;
+	public boolean modified;
 	public ArrayList<Continent> continents;
 	public int countryNum;
 	public Map<Integer,ArrayList<Country>> countries;
-	public Map<Integer,ArrayList<Integer>> adjacencyList;//add
-	
+	public Map<Integer,ArrayList<Integer>> adjacencyList;
+
 	public RiskMap(String name){
 		this.globalIndex = 1;
 		this.riskMapName = name;
+		this.author = "Invincible Team Four";
+		this.warn = "yes";
+		this.wrap = "no";
+		this.image = "none";
+		this.scroll = "no";
+		this.modified = false;
 		this.continents = new ArrayList<Continent>();
 		this.countryNum = 0;
 		this.countries = new HashMap<Integer,ArrayList<Country>>();
-		this.adjacencyList = new HashMap<Integer,ArrayList<Integer>>();//add
-	}	
-	
+		this.adjacencyList = new HashMap<Integer,ArrayList<Integer>>();
+	}
+
 	public Country findCountry(String countryName) {
-		for (ArrayList<Country> loopList : countries.values()) {  
+		for (ArrayList<Country> loopList : countries.values()) {
 			for (Country loopCountry:loopList){
 				if (loopCountry.countryName.equals(countryName)){
 					return loopCountry;
-				}	
+				}
 			}
 		}
 		return null;
 	}
-	
+
 	public Continent findContinent(String continentName) {
 		for (Continent loopContinent:continents){
 			if (loopContinent.continentName.equals(continentName)){
 				return loopContinent;
-			}	
+			}
 		}
 		return null;
-	}	
-	
+	}
+
 	public boolean addContinent(String continentName){
-		if (findContinent(continentName)!=null){ 	
+		if (findContinent(continentName)!=null) {
 			JOptionPane.showMessageDialog(null,"Continnet '"+continentName+"' already exists");
 			return false;
-		}	
-		else {
-        	Continent newContinent = new Continent(globalIndex++,continentName);
-        	continents.add(newContinent);
-        	countries.put(newContinent.continentID, new ArrayList<Country>());
-        	return true;
 		}
+		Continent newContinent = new Continent(globalIndex++,continentName);
+		continents.add(newContinent);
+		countries.put(newContinent.continentID, new ArrayList<Country>());
+		this.modified = true;
+		return true;
 	}
-	
+
 	public boolean addCountry(String countryName,String continentName){
 		int continentID = findContinent(continentName).continentID;
 		ArrayList<Country> targetCountryList = countries.get(continentID);
 		if (targetCountryList==null) {
 			JOptionPane.showMessageDialog(null,"Continnet '"+continentName+"' does not exists");
 			return false;
-		}	
-		
+		}
 		if (findCountry(countryName)!=null){
 			JOptionPane.showMessageDialog(null,"Country '"+countryName+"' already exists");
 			return false;
 		}
-		
+
 		Country newCountry = new Country(globalIndex++,countryName,findContinent(continentName));
 		targetCountryList.add(newCountry);
-		adjacencyList.put(newCountry.countryID, new ArrayList<Integer>());//add
-		
+		adjacencyList.put(newCountry.countryID, new ArrayList<Integer>());
 		countryNum++;
+		this.modified = true;
 		return true;
 	}
-	
+
 	public boolean addConnection(String countryNameFrom,String countryNameTo){
 		Country fromCountry = findCountry(countryNameFrom);
 		if (fromCountry==null){
-			JOptionPane.showMessageDialog(null,"Country  '"+countryNameFrom+"' does not exists");	
+			JOptionPane.showMessageDialog(null,"Country  '"+countryNameFrom+"' does not exists");
 			return false;
-		}		
+		}
 		Country toCountry = findCountry(countryNameTo);
 		if (toCountry==null){
 			JOptionPane.showMessageDialog(null,"Country  '"+countryNameTo+"' does not exists");
 			return false;
-		}			
+		}
 		adjacencyList.get(fromCountry.countryID).add(toCountry.countryID);
 		adjacencyList.get(toCountry.countryID).add(fromCountry.countryID);
+		this.modified = true;
 		return true;
 	}
-	
+
+	public void addCompletedConnection(){
+		for (int loopCountryID:adjacencyList.keySet()){
+			ArrayList<Integer> loopCountryList = adjacencyList.get(loopCountryID);
+			loopCountryList.clear();
+			for (int candidateID:adjacencyList.keySet()){
+				if (candidateID!=loopCountryID){
+					loopCountryList.add(candidateID);
+				}
+			}
+		}
+		this.modified = true;
+	}
+
 	public boolean removeConnection(String countryNameFrom,String countryNameTo){
 		Country fromCountry = findCountry(countryNameFrom);
 		if (fromCountry==null){
-			JOptionPane.showMessageDialog(null,"Country  '"+countryNameFrom+"' does not exists");	
+			JOptionPane.showMessageDialog(null,"Country  '"+countryNameFrom+"' does not exists");
 			return false;
-		}		
+		}
 		Country toCountry = findCountry(countryNameTo);
 		if (toCountry==null){
 			JOptionPane.showMessageDialog(null,"Country  '"+countryNameTo+"' does not exists");
 			return false;
-		}			
+		}
 		adjacencyList.get(fromCountry.countryID).remove(adjacencyList.get(fromCountry.countryID).indexOf(toCountry.countryID));
 		adjacencyList.get(toCountry.countryID).remove(adjacencyList.get(toCountry.countryID).indexOf(fromCountry.countryID));
+		this.modified = true;
+		return true;
+	}
+
+	public void removeAllConnection(){
+		for (ArrayList<Integer> loopCountryList:adjacencyList.values()){
+			loopCountryList.clear();
+		}
+		this.modified = true;
+	}
+
+	public boolean renameCountry(String countryName, String newName){
+		Country changeCountry = findCountry(countryName);
+		if(changeCountry == null){
+			JOptionPane.showMessageDialog(null,"Country  '"+countryName+"'  you want change does not exists");
+			return false;
+		}
+		if (findCountry(newName)!=null){
+			JOptionPane.showMessageDialog(null,"Country '"+newName+"' already exits");
+			return false;
+		};
+		changeCountry.changeName(newName);
+		this.modified = true;
 		return true;
 	}
 
@@ -126,6 +170,33 @@ public class RiskMap {
 		}
 		adjacencyList.remove(deleteCountry.countryID);
 		countryNum--;
+		this.modified = true;
+		return true;
+	}
+
+	public boolean renameContinent(String continentName, String newName){
+		Continent changeContinent = findContinent(continentName);
+		if(changeContinent == null){
+			JOptionPane.showMessageDialog(null,"Continent  '"+continentName+"'  you want change does not exists");
+			return false;
+		}
+		if (findContinent(newName)!=null){
+			JOptionPane.showMessageDialog(null,"Continent '"+newName+"' already exits");
+			return false;
+		};
+		changeContinent.changeName(newName);
+		this.modified = true;
+		return true;
+	}
+
+	public boolean changeContinentControl(String continentName, int controlNum){
+		Continent changeContinent = findContinent(continentName);
+		if(changeContinent == null){
+			JOptionPane.showMessageDialog(null,"Continent  '"+continentName+"'  you want change does not exists");
+			return false;
+		}
+		changeContinent.changeControlNum(controlNum);
+		this.modified = true;
 		return true;
 	}
 
@@ -143,6 +214,7 @@ public class RiskMap {
 		else{
 			continents.remove(deleteContinent);
 			countries.remove(deleteContinent.continentID);
+			this.modified = true;
 			return true;
 		}
 	}
@@ -154,6 +226,7 @@ public class RiskMap {
 			return false;
 		}
 		Continent movetoContinent = findContinent(toContinentName);
+
 		if(movetoContinent == null){
 			JOptionPane.showMessageDialog(null,"Continent  '"+toContinentName+"'  you want move to does not exists");
 			return false;
@@ -161,28 +234,7 @@ public class RiskMap {
 		countries.get(moveCountry.belongToContinent.continentID).remove(moveCountry);
 		countries.get(movetoContinent.continentID).add(moveCountry);
 		moveCountry.belongToContinent = movetoContinent;
-
-		return true;
-	}
-
-
-	public boolean renameCountry(String countryName, String newName){
-		Country changeCountry = findCountry(countryName);
-		if(changeCountry == null){
-			JOptionPane.showMessageDialog(null,"Country  '"+countryName+"'  you want change does not exists");
-			return false;
-		}
-		changeCountry.changeName(newName);
-		return true;
-	}
-
-	public boolean renameContinent(String continentName, String newName){
-		Continent changeContinent = findContinent(continentName);
-		if(changeContinent == null){
-			JOptionPane.showMessageDialog(null,"Continent  '"+continentName+"'  you want change does not exists");
-			return false;
-		}
-		changeContinent.changeName(newName);
+		this.modified = true;
 		return true;
 	}
 
