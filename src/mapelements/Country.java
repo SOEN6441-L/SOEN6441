331a4206a667a1dade1079775aff0918e9543a49
Nobitakun151 <32436934+LiChongWen@users.noreply.github.com>
2017-10-14 20:54:11 -0400

    ReinforceArmyAchieve

--- src/mapelements/Country.java
+++ src/mapelements/Country.java
@@ -5,12 +5,16 @@ public class Country {
 	public String countryName;
 	public Continent belongToContinent;
 	public boolean flagBFS;
+
+	public int armyNumber;
 	
 	public Country(int ID, String name, Continent continent){
 		this.countryID = ID;
 		this.countryName = name;
 		this.belongToContinent = continent;
 		flagBFS = false;
+
+		this.armyNumber = 0;
 	}
 	
 }
