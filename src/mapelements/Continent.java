ca16c091614a5b67a75b1d0d63008837ed354530
raphealshirley <32117886+raphealshirley@users.noreply.github.com>
2017-10-14 22:11:03 -0400

    Add changeCcontrolNum method
    Add changeCcontrolNum method

--- src/mapelements/Continent.java
+++ src/mapelements/Continent.java
@@ -4,13 +4,17 @@ public class Continent {
 	public int continentID;
 	public String continentName;
 	public int controlNum;
-	
-	public Continent(int ID,String name){
+
+	public Continent(int ID, String name) {
 		this.continentID = ID;
 		this.continentName = name;
 	}
-	
-	public void changeName(String name){
+
+	public void changeName(String name) {
 		this.continentName = name;
 	}
+	public void changeControlNum(int control){
+		this.controlNum = control;
+	}
+
 }
