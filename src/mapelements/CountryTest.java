c2c0aa3f67d9daad7cd3028455993b845e2a920d
raphealshirley <32117886+raphealshirley@users.noreply.github.com>
2017-10-19 15:39:27 -0400

    country test

--- src/mapelements/CountryTest.java
+++ src/mapelements/CountryTest.java
@@ -7,10 +7,12 @@ import org.junit.Test;
 
 public class CountryTest {
 	private Country country;
+	private Continent continent;
 	
 	@Before
 	public void setCountry(){
 		country = new Country(1,"2",null);
+		continent = new Continent(1,"1");
 		System.out.println("Start test");
 	}
 	
@@ -20,5 +22,12 @@ public class CountryTest {
 		assertEquals("1",country.countryName);
 		System.out.println("Test change name success");
 	}
-
+	
+	@Test
+	public void testChangeContinent(){
+		country.changeContinent(continent);
+		assertEquals("1",country.belongToContinent.continentName);
+		System.out.println("Test changeContinet success");
+	}
+	
 }
