24295fb34ca21473496a67eaceef319f817351bb
Nobitakun151 <32436934+LiChongWen@users.noreply.github.com>
2017-11-08 10:44:26 -0500

    javaDoc

--- src/gamemodelstest/FortificationPhaseTest.java
+++ src/gamemodelstest/FortificationPhaseTest.java
@@ -1,4 +1,5 @@
 package gamemodelstest;
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
 
 import java.awt.*;
--- src/gameviews/FortificationPhaseView.java
+++ src/gameviews/FortificationPhaseView.java
@@ -27,8 +27,8 @@ import gamemodels.RiskGameModel;
 import mapmodels.CountryModel;
 
 /**
- * This class is the implementation of reinforcement phase in the Risk.
- * <p> The ReinforcementPhase class will allow players add armies to their<br>
+ * This class is the implementation of Fortification phase in the Risk.
+ * <p> The FortificationPhaseView class will allow players add armies to their<br>
  * to their country, based on the countries under control.</p >
  *
  */
@@ -61,7 +61,7 @@ public class FortificationPhaseView extends JDialog{
     public int state=0; //0-Cancel, 1-confirm
 
     /**
-     * Constructor of class ReinforcePhaseView to generate reinforce phase UI
+     * Constructor of class FortificationPhaseView to generate reinforce phase UI
      * @param player The player that who is in turn
      * @param game The game reinforce phase is in
      */
