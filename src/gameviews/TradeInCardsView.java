a426c30924af026387140818a901b7bf5c5a5bd6
raphealshirley <32117886+raphealshirley@users.noreply.github.com>
2017-11-21 22:18:18 -0500

    add javadoc

--- src/gameviews/TradeInCardsView.java
+++ src/gameviews/TradeInCardsView.java
@@ -37,8 +37,11 @@ public class TradeInCardsView extends JFrame implements Observer {
 			dispose();
 		}
 	};
-    
-    public TradeInCardsView(){
+
+	/**
+	 * This is view window for trading in cards
+	 */
+	public TradeInCardsView(){
         //this.setLocation((screenWidth-500)/3, (screenHeight-300)/3);
         this.setSize(400,230);
         this.setLayout(null);
@@ -67,7 +70,12 @@ public class TradeInCardsView extends JFrame implements Observer {
         this.setVisible(false);
     }
 
-    public void update(Observable obs, Object x){
+	/**
+	 * Rewrite update method.
+	 * @param obs observable
+	 * @param x observed object
+	 */
+	public void update(Observable obs, Object x){
     	int type = (Integer) x;
     	if (type == 4){//update player phase info
     		Label1.setText(MessageFormat.format("{0} now has {1}",((PlayerModel)obs).getName(),((PlayerModel)obs).getCardsString(1)));
@@ -100,8 +108,11 @@ public class TradeInCardsView extends JFrame implements Observer {
        		worker.execute();
     	}
     }
-    
-    public void closeTradeInCardsView(){
+
+	/**
+	 * This is to close trade in cards view
+	 */
+	public void closeTradeInCardsView(){
         try {
             Thread.sleep(6000);
             this.dispose();
@@ -110,19 +121,4 @@ public class TradeInCardsView extends JFrame implements Observer {
         }
     }
 
-    public static void main(String args[]){
-        TradeInCardsView T = new TradeInCardsView();
-        PlayerModel P = new PlayerModel("12",Color.RED,null);
-
-        T.setVisible(true);
-
-        P.addObserver(T);
-        int[] cards = {6,2,3};
-        P.setCards(cards);
-
-        int[] cards2 = {1,3,3};
-        P.setCards(cards2);
-
-        T.closeTradeInCardsView();
-    }
 }
