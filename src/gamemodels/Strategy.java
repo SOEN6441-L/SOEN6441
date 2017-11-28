ff5748bd4a9e678399ead14415425cf0da8be46e
jingyu6lu <lujingyu91@gmail.com>
2017-11-28 11:01:50 -0500

    Update Strategy.java
    2017-11-28

--- src/gamemodels/Strategy.java
+++ src/gamemodels/Strategy.java
@@ -1,7 +1,20 @@
 package gamemodels;
 
 public interface Strategy {
+	/**
+	 * Method reinforcementPhase()
+	 * @param player
+	 */
 	public void reinforcementPhase(PlayerModel player);
-	public void attackPhase(PlayerModel player);
+	/**
+	 * Method attackPhase()
+	 * @param player
+	 * @return 1- no possible attack 2-finished by player
+	 */
+	public int attackPhase(PlayerModel player);
+	/**
+	 * Method fortificationPhase()
+	 * @param player
+	 */
 	public void fortificationPhase(PlayerModel player);
 }
